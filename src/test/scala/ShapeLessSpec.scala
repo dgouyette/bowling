27c1a19493f080b19426d7f4fc9f915451d172cd
dgouyette <damien.gouyette@gmail.com>
2015-09-01 13:35:03 +0200

    ony one test is not working (yet)

--- src/test/scala/ShapeLessSpec.scala
+++ src/test/scala/ShapeLessSpec.scala
@@ -1,105 +1,144 @@
 import org.scalatest._
 import shapeless._
 
+import scala.annotation.tailrec
+
 
 object Bowling {
 
-type Manche = Int :: Int :: HNil
+  sealed trait MancheT
 
-type DerniereManche = Int :: Int :: Int ::  HNil
+  case class Manche(a: Int, b: Int) extends MancheT
 
-type JeuUneManche = Manche :: HNil
-type JeuDeuxManches = Manche :: Manche ::  HNil
-type JeuTroisManches = Manche :: Manche :: Manche ::  HNil
-type JeuMancheMixtes = Manche :: Manche :: DerniereManche :: HNil
-type JeuOriginal =  Manche :: Manche :: Manche :: Manche  :: Manche :: Manche  ::Manche :: Manche :: Manche :: DerniereManche :: HNil
+  case class DerniereManche(a: Int, b: Int, c: Int) extends MancheT
 
-  trait Jeu[L <: HList]
+  object MancheT {
+    def unapply(m: MancheT): Option[(Int, Int, Option[Int])] = {
+      m match {
+        case Manche(a, b) => Some(a, b, None)
+        case DerniereManche(a, b, c) => Some(a, b, Some(c))
+      }
+    }
+
+    def sum(m: MancheT): Int = {
+      unapply(m).map { p => p._1 + p._2 + p._3.getOrElse(0)
+      }.getOrElse(0)
+    }
+  }
+
+
+  type JeuUneManche = Manche :: HNil
+  type JeuDeuxManches = Manche :: Manche :: HNil
+  type JeuTroisManches = Manche :: Manche :: Manche :: HNil
+  type JeuMancheMixtes = Manche :: Manche :: DerniereManche :: HNil
+  type JeuOriginal = Manche :: Manche :: Manche :: Manche :: Manche :: Manche :: Manche :: Manche :: Manche :: DerniereManche :: HNil
 
   object Score extends Poly1 {
-  implicit def caseManche  = at[Manche](manche => manche.head + manche.tail.reduceLeft(Add))
-  implicit def caseDerniereManche  = at[DerniereManche](manche => manche.head + Score(manche.tail))
-  implicit def caseJeu = at[JeuUneManche](jeu =>  Score(jeu.head))
-  implicit def caseJeu2 = at[JeuDeuxManches](jeux2Manches2Score )
-  implicit def caseJeu3 = at[JeuTroisManches](jeu3ManchesScore)
+    implicit def caseMancheT = at[MancheT] {
+      case Manche(a, b) => a + b
+      case DerniereManche(a, b, c) => a + b + c
+    }
 
+    implicit def caseManche = at[Manche](MancheT.sum)
+    implicit def caseDerniereManche = at[DerniereManche](MancheT.sum)
 
-  def jeux2Manches2Score: (Manche :: Manche :: HNil) => Int = {
-    case (h1 :: h2 :: HNil) :: (t1 :: t2 :: HNil) :: _ if h1 == 10 => h1 + t1 + t1 + t2 + t2
-    case (h1 :: h2 :: HNil) :: (t1 :: t2 :: HNil) :: _ if h1 + h2 == 10 => h1 + h2 + t1 + t1 + t2
-    case jeu => Score(jeu.head) + jeu.tail.map(Score).reduceLeft(Add)
+    implicit def caseJeu = at[JeuUneManche](jeux2Manches2Score)
+    implicit def caseJeu2 = at[JeuDeuxManches](jeux2Manches2Score)
+    implicit def caseJeu3 = at[JeuTroisManches](jeux2Manches2Score)
+    implicit def caseJeuOriginal = at[JeuOriginal](jeux2Manches2Score)
+
+    def jeux2Manches2Score[L]: (HList) => Int = {
+      case DerniereManche(a,b,c) :: _  if a == 10 && b == 10 && c == 10 => a + strike(Manche(b,c)) + b + strike(Manche(c,0)) + c
+      case (Manche(h1, h2)) :: (m2: MancheT) :: tail if h1 == 10 => h1 + strike(m2) + jeux2Manches2Score(m2 :: tail)
+      case (Manche(h1, h2)) :: (m2: MancheT) :: tail if h1 + h2 == 10 => h1+h2  + spair(m2)  +   jeux2Manches2Score(m2 :: tail)
+
+      case (m1: MancheT) :: t => Score(m1) + jeux2Manches2Score(t)
+      case HNil => 0
+    }
   }
 
 
+  def spair[L <: MancheT](m: MancheT): Int = {
+    m match {
+      case Manche(a, b) => a
+      case DerniereManche(a, b, c) => a
 
-    def jeu3ManchesScore : (Manche :: Manche :: Manche :: HNil ) => Int  = {
-    case (h1 :: h2 :: HNil) :: (t1 :: t2 :: HNil) :: tail if h1 == 10 => h1 + t1 + t1 + t2 + t2 + Score(tail)
-    case (h1 :: h2 :: HNil) :: (t1 :: t2 :: HNil) :: tail if h1 + h2 == 10 & t1 + t2 == 10 => h1 + h2 + t1 + t1 + t2 + tail.head.head +  Score(tail)
-    case (h1 :: h2 :: HNil) :: (t1 :: t2 :: HNil) :: tail if h1 + h2 == 10 => h1 + h2 + t1 + t1 + t2 + Score(tail)
-    case jeu => Score(jeu.head) + Score(jeu.tail)
+    }
   }
-}
 
+  def strike[L <: MancheT](m: MancheT): Int = {
+    m match {
+      case Manche(a, b) => a+b
+      case DerniereManche(a, b, c) => a+b
 
-
-
-object Add extends Poly2 {
-  implicit def caseInt = at[Int, Int]( _ + _ )
-}
-
-
+    }
+  }
 }
 class ShapeLessSpec extends FunSuite with ShouldMatchers {
 
   import Bowling._
 
+  test("une spair doit retourner la première boule d'un Manche") {
+    spair(Manche(1, 2)) shouldEqual 1
+    spair(DerniereManche(5, 1, 2)) shouldEqual 5
+  }
+
+  test("un strike doit retourner les  deux boules d'une manche Manche"){
+    strike(Manche(1,2))  shouldEqual 1+2
+    strike(DerniereManche(5,1,2))  shouldEqual 5 + 1
+  }
+
+  test("deux strikes de suite égal  30"){
+    Score(Manche(10, 0) :: Manche(10, 0) :: HNil) shouldEqual 30
+  }
+
   test("Une manche 0 + 0  doit avoir un score de 0"){
-    Score(0  :: 0 :: HNil) shouldEqual 0
+    Score(Manche(0, 0)) shouldEqual 0
   }
 
   test("Une manche 3 + 1 doit avoir un score de 4") {
-    Score(3  :: 1 :: HNil) shouldEqual 4
+    Score(Manche(3, 1)) shouldEqual 4
   }
 
   test("Une manche 0 + 0 + 0 doit avoir un score de 0" ) {
-    Score(0  :: 0 ::  0 ::  HNil) shouldEqual 0
+    Score(DerniereManche(0, 0, 0)) shouldEqual 0
   }
 
   test("Deux manches 0 + 10 + 1 + 2  doit avoir un score de 0+10+1 + 1+2") {
-    Score((0  ::  10  :: HNil) :: ( 1 ::  2 :: HNil)  :: HNil) shouldEqual 0 + 10 + 1 + 1 + 2
+    Score(Manche(0 ,  10  ) :: Manche(1 ,  2 )  :: HNil) shouldEqual 0 + 10 + 1 + 1 + 2
   }
 
-  test("Deux manches  1 + 10 + 1 + 2  doit avoir un score de 0+10 + 1+1 + 2") {
-    Score((0  ::  10  :: HNil) :: ( 1 ::  2 :: HNil) :: HNil) shouldEqual 0+10 + 1+1 + 2
+  test("Deux manches  0+10 + 1+2  doit avoir un score de 0+10 + 1+1 + 2") {
+    Score(Manche(0  ,  10  ) :: Manche( 1 ,  2 ) :: HNil) shouldEqual 0+10 + 1+1 + 2
   }
 
   test("Trois manches 1 + 2 +  + 1 +  2  +  1 + 2 doit avoir un score de  1+2 + 1+2 + 1+2") {
-    Score((1 :: 2 :: HNil) :: (1 :: 2 :: HNil) :: (1 :: 2 :: HNil) :: HNil) shouldEqual 1+2 + 1+2 + 1+2
+    Score(Manche(1, 2) :: Manche(1, 2) :: Manche(1, 2) :: HNil) shouldEqual 1+2 + 1+2 + 1+2
   }
 
 
   test("Trois manche 0+10 + 5+5 +  1+2 doit avoir un score de 0+10+ 5+5+5 + 1+1 + 2") {
-    Score((0 :: 10 :: HNil) :: (5 :: 5 :: HNil) :: (1 :: 2 :: HNil) :: HNil) shouldEqual 0+10+ 5+5+5 + 1+1 + 2
+    Score(Manche(0 , 10 ) :: Manche(5, 5) :: Manche(1 , 2 ) :: HNil) shouldEqual 0+10 + 5+5+5 + 1+1 + 2
   }
 
   test("Une manche 3+1 + 0+10 + 5+1 doit avoir un score de 3+1 + 0+10+5 + 5+1") {
-    Score((3 :: 1 :: HNil) ::  (0 ::  10 :: HNil) :: (5 ::  1 :: HNil ) :: HNil ) shouldEqual 3+1 + 0+10+5 + 5+1
+    Score(Manche(3 , 1) ::  Manche(0 ,  10 ) :: Manche(5 ,  1 ) :: HNil ) shouldEqual 3+1 + 0+10+5 + 5+1
   }
 
   test("Une manche 10 + 2 + 5  doit avoir un score de 10 + 2+5 + 2+5") {
-    Score((10 :: 0 :: HNil)   :: (2 ::  5 :: HNil)  :: HNil ) shouldEqual 10 + 2 +2 + 5+5
+    Score(Manche(10 , 0 )   :: Manche(2 ,  5 )  :: HNil ) shouldEqual 10 + 2 +2 + 5+5
   }
 
-  /**test("Une manche 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5+5 doit avoir un score de 5+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5+5"){
-    val spair  = 5 :: 5 :: HNil
-    val dernierSpair = 5 :: 5 :: 0 :: HNil
-    Score(spair :: spair :: spair :: spair :: spair :: spair :: spair :: spair :: spair :: dernierSpair :: HNil) shouldEqual 5+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5+5
-  }**/
-
-/**
-  "Une manche 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 " should " doit avoir un score de 300" in {
-    score(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10) shouldEqual 300
+  test("Une manche 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5 + 5+5+5 doit avoir un score de 5+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5 + 5*2+5+5"){
+    val spair  = Manche(5 , 5 )
+    val dernierSpair = DerniereManche(5 , 5 , 5)
+    Score(spair :: spair :: spair :: spair :: spair :: spair :: spair :: spair :: spair :: dernierSpair :: HNil) shouldEqual 5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5 + 5+5+5+5
   }
-**/
+
+  test("Une manche 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 + 10 doit doit avoir un score de 300") {
+    val strike = Manche(10, 0)
+    val dernierStrike = DerniereManche(10, 10, 10)
+    Score(strike :: strike :: strike :: strike :: strike :: strike :: strike :: strike :: strike :: dernierStrike :: HNil) shouldEqual 300
+  }
 
 }
